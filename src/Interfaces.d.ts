15cc99435f45dada3f96127bd2ecd2e7cce0d70e
KylixMedusa <ayush.agarwalmng@gmail.com>
2021-01-03 14:50:28 +0530

    Added blog card interface

---
+++ src/Interfaces.d.ts
@@ -0,0 +1,10 @@
+export interface BlogCard{
+    blog_id:string,
+    author:string,
+    author_icon:string,
+    img:string,
+    title:string,
+    tag:string,
+    createdAt:Date,
+    timeRead:string
+}
